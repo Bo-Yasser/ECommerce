200d51cd63e84e530812c5202038096b809e9a9f
Mohamed Yaser <mohamedyasser1960@gmail.com>
2026-07-20 11:47:59 +0300

    chore: Setup EF Core DbContext and SQL Server

---
+++ ECommerce.Infrastructure/Data/DbContexts/StoreDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infrastructure.Data.DbContexts;
+
+public class StoreDbContext(DbContextOptions<StoreDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+
+}
--- ECommerce.Infrastructure/DependencyInjection.cs
+++ ECommerce.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using ECommerce.Infrastructure.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ECommerce.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration config)
     {
+        services.AddDbContext<StoreDbContext>(options =>
+        {
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection"))
+                    .EnableSensitiveDataLogging();
+        });
         return services;
     }
 }

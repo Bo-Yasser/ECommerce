3bd633adb54d74e3f357da5ae57ae41d0dd064bf
Mohamed Yaser <mohamedyasser1960@gmail.com>
2026-07-24 18:46:17 +0300

    feat: Implement BrandsController with GetAll endpoint

---
+++ ECommerce.API/Controllers/BrandsController.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.UseCases.ProductBrands.Dtos;
+using ECommerce.UseCases.ProductBrands.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers;
+
+public class BrandsController(
+    GetAllBrandsQuery getAllBrandsQuery) : ApiControllerBase
+{
+    [HttpGet] // GET api/brands
+    public async Task<ActionResult<IReadOnlyList<GetAllBrandsResponse>>> GetAll(CancellationToken ct = default)
+    {
+        var result = await getAllBrandsQuery.ExecuteAsync(ct);
+        return Ok(result.Value);
+    }
+}

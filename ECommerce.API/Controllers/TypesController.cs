cab4022bdc7acb570a95b3235aa4c99489013f4e
Mohamed Yaser <mohamedyasser1960@gmail.com>
2026-07-24 18:46:30 +0300

    feat: Implement TypesController with GetAll endpoint

---
+++ ECommerce.API/Controllers/TypesController.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.UseCases.ProductTypes.Dtos;
+using ECommerce.UseCases.ProductTypes.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers;
+
+public class TypesController(GetAllTypesQuery getAllTypesQuery) : ApiControllerBase
+{
+    [HttpGet] // GET api/types
+    public async Task<ActionResult<IReadOnlyList<GetAllTypesResponse>>> GetAll(CancellationToken ct = default)
+    {
+        var result = await getAllTypesQuery.ExecuteAsync(ct);
+        return Ok(result.Value);
+    }
+
+}
